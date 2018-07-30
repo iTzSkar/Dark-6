00a8dc8d2a2b57fe63f7812911cd587962b66db0
iTzSkar <38841221+iTzSkar@users.noreply.github.com>
2018-07-30 13:23:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '@.Savage'
+const prefix = '1'
 
-console.log("Welcome Savage.");
- 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
 client.on('message', message => {
-  if(message.content.startsWith(prefix + "join")) {
-  message.member.voiceChannel.join();
-  }
-  });
+ if(message.content.startsWith(prefix + "join")) {
+message.member.voiceChannel.join().catch(e => message.channel.send(e));
+}
+});
+
   
 
 client.login(process.env.BOT_TOKEN); 
